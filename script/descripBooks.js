70680adcf70f21fd35d31e01429551310401e90f
dangkay <kaylindang345@gmail.com>
2025-11-28 15:34:51 -0500

    Updated descripBooks.js

--- script/descripBooks.js
+++ script/descripBooks.js
@@ -2,109 +2,109 @@ const books = {
       invisible: {
         title: "The Invisible Life of Addie LaRue ",
         author: "V.E. Schwab",
-        img: "img/trending-booke-one.png",
+        img: "img/book/trending-booke-one.png",
         desc: "Addie LaRue makes a deal with the devil for eternal life, but the catch is that everyone she meets forgets her the moment she’s out of sight. For centuries, she wanders through history, unseen and unremembered, until one day, someone finally remembers her. This is a haunting, romantic story about identity and legacy and what it means to truly be seen. This book was so emotional. Addie’s story will stay with me forever. Took me a minute to get into, but once I did, wow. The writing is pure magic. Made me want to live a thousand lives just to leave a mark like Addie."
       },
       fearless: {
         title: "Fearless",
         author: "Lauren Roberts",
-        img: "img/trending-booke-two.png",
+        img: "img/book/trending-booke-two.png",
         desc: "Following the events of Powerless, Paedyn and Kai face a kingdom on the edge of chaos. Loyalties are tested, secrets unravel, and the line between love and duty blurs more than ever. The romance is tense, the stakes are higher, and every chapter feels like it’s ready to explode. It’s an emotional rollercoaster that leaves you desperate for the next book. "
       },
       silent: {
         title: "The Silent Patient",
         author: "Alex Michaelides",
-        img: "img/trending-booke-four.png",
+        img: "img/book/trending-booke-four.png",
         desc: "When famous painter Alicia Berenson shoots her husband and stops speaking entirely, she is sent to a secure psychiatric facility. Theo Faber, a psychotherapist, becomes obsessed with finding out the truth behind her silence."
       },
       seven: {
         title: "The Seven Year Slip",
         author: "Ashley Poston",
-        img: "img/trending-booke-three.png",
+        img: "img/book/trending-booke-three.png",
         desc: "A book editor, grieving the death of her partner, finds that her apartment sometimes connects her to another timeline, seven years in the past. When she meets a man living in that version of her apartment, their lives intertwine in ways neither can explain. It’s emotional, clever, and quietly magical. Ashley Poston perfectly captures grief, healing, and a love that transcends time."
       },
       women: {
         title: "The Women",
         author: "Kristin Hannah",
-        img: "img/trending-booke-five.png",
+        img: "img/book/trending-booke-five.png",
         desc: "Set during the Vietnam War, The Women follows Frances “Frankie” McGrath, a nurse who enlists to serve and discovers the brutal realities of war. It’s a story of courage, trauma, and sisterhood, showing the untold struggles of women who fought to be seen."
       },
         evelyn: {
         title: "The Seven Husbands of Evelyn Hugo",
         author: "Taylor Jenkins Reid",
-        img: "img/trending-booke-six.png",
+        img: "img/book/trending-booke-six.png",
         desc: "When Hollywood legend Evelyn Hugo finally agrees to tell the story of her glamorous and scandalous life, she tells it to Monique Grant, an unknown journalist. As Evelyn recounts her glamorous and scandalous past, her seven marriages, her secrets, and her true love, Monique learns more about herself than she expects. It’s dramatic, emotional, and layered."
       },
       bride: {
         title: "Bride",
         author: "Ali Hazelwood",
-        img: "img/trending-booke-seven.png",
+        img: "img/book/trending-booke-seven.png",
         desc: "Misery Lark, the only daughter of a powerful vampire family, is forced into a marriage alliance with a werewolf alpha. What starts as political convenience turns into an unexpectedly steamy, emotional romance. Ali Hazelwood delivers her signature mix of wit, sciencey humor, and swoon-worthy chemistry but in a supernatural twist. It’s fast-paced and fun. "
       },
       voice: {
         title: "Archer's Voice",
         author: "Mia Sheridan",
-        img: "img/trending-booke-eight.png",
+        img: "img/book/trending-booke-eight.png",
         desc: "Bree Prescott moves to a small town in search of peace, but what she finds is Archer Hale, a quiet, isolated man with a painful past. Their connection grows slowly, built on trust, silence, and understanding. This is an emotional, tender love story about healing and vulnerability. Have tissues ready. "
       },
       funny: {
         title: "Funny Story",
         author: "Emily Henry",
-        img: "img/trending-booke-nine.png",
+        img: "img/book/trending-booke-nine.png",
         desc: "Daphne always loved the way her fiancé, Peter, told their story. How they met (on a blustery day), fell in love (over an errant hat), and moved back to his lakeside hometown to begin their life together. He really was good at telling it... right up until the moment he realized he was actually in love with his childhood best friend Petra."
       },
       normal: {
         title: "Normal People",
         author: "Sally Rooney",
-        img: "img/trending-booke-ten.png",
+        img: "img/book/trending-booke-ten.png",
         desc: "Connell and Marianne grow up in the same small Irish town but come from different worlds. Their relationship, sometimes romantic, sometimes distant, follows them through years of love, miscommunication, and emotional growth. Sally Rooney does a great job at showing the intensity of young adulthood. "
       },
       assistant: {
         title: "Assistant to the Villian",
         author: "Hannah Nicole Maehrer",
-        img: "img/trending-booke-eleven.png",
+        img: "img/book/trending-booke-eleven.png",
         desc: "Evie Sage accidentally lands a job working for the realm’s most notorious villain, and finds out that being his assistant involves as much chaos as it does paperwork. Quirky, funny, and packed with banter and slow-burn tension. It’s a perfect mix of fantasy adventure and office rom-com energy.  "
       },
       unmaking: {
         title: "The Unmaking of June Farrow",
         author: "Adrienne Young",
-        img: "img/trending-booke-twelve.png",
+        img: "img/book/trending-booke-twelve.png",
         desc: "June Farrow and the other women in her family are haunted by strange visions and whispers. When she discovers a mysterious door, she steps into another world and reveals secrets that could change everything she knows. This fantasy romance is the perfect casual read."
       },
       housemaid: {
         title: "The Housemaid",
         author: "Freida McFadden",
-        img: "img/trending-booke-thirteen.png",
+        img: "img/book/trending-booke-thirteen.png",
         desc: "Millie takes a job working for a wealthy family but soon realizes something interesting about the house. There’s something about it and the people in it that isn’t quite right. There are locked doors, lies, and tension. A fast-paced story with a dark twist. "
       },
       little: {
         title: "Little Women",
         author: "Louise Mary Alcott",
-        img: "img/trending-booke-fourteen.png",
+        img: "img/book/trending-booke-fourteen.png",
         desc: " The four March sisters, Meg, Jo, Beth, and Amy grew up during the Civil War and learned about love, family, and independence. Each sister has her own dreams and struggles. The warm, heartfelt, and timeless story is relatable in some way for everyone. "
       },
       broken: {
         title: "Once Upon A Broken Heart",
         author: "Stephanie Garber",
-        img: "img/trending-booke-fifteen.png",
+        img: "img/book/trending-booke-fifteen.png",
         desc: "The pink haired Evangeline Fox makes a deal with the charming but dangerous Prince of Hearts, Jacks, after her love story ended in betrayal. All magic comes with a price, so Evangeline finds herself caught in a web of love, lies, and fate. "
       },
       educated: {
         title: "Educated",
         author: "Tara Westover",
-        img: "img/trending-booke-sixteen.png",
+        img: "img/book/trending-booke-sixteen.png",
         desc: "Growing up in a strict, isolated family with no formal education made it hard for Tara Westover on her journey to earn a PhD from Cambridge, but she made it happen. In her memoir, her story is raw, powerful, and deeply inspiring. Read as she redefines herself and grows into the person she is today. "
       },
       powerless: {
         title: "Powerless",
         author: "Lauren Roberts",
-        img: "img/trending-booke-seventeen.png",
+        img: "img/book/trending-booke-seventeen.png",
         desc: "Paedyn Gray hides in plain sight, an ordinary in a world of elites. When she crosses paths with Kai, a powerful prince whose mission is to rid the world of ordinaries, both of their lives turn upside down. It’s a thrilling enemies to lovers fantasy with rebellion, secrets, and serious tension. "
       },
       bell: {
         title: "The Bell Jar",
         author: "Sylvia Plath",
-        img: "img/trending-booke-eighteen.png",
+        img: "img/book/trending-booke-eighteen.png",
         desc: "Under the pressure of expectations and mental illness, Esther Greenwood struggles to have it all. This semi-autobiographical novel is beautifully written and very honest. The emotional exploration of identity explores what it’s like to feel trapped in your own life. "
       },
     };
